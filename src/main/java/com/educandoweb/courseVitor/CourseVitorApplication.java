4c21c912ed1a29da739b9fe0a8097ac743947c83
vitor poerschke <vitordrakomanico@gmail.com>
2024-11-05 15:49:54 -0300

    projeto criado

---
+++ src/main/java/com/educandoweb/courseVitor/CourseVitorApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseVitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseVitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseVitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/courseVitor/CourseVitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseVitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseVitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
